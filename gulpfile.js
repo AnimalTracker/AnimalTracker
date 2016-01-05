7a9c4c2c0835d2594d3c49cab78788f7fa331359
Fanaen <contact@fanaen.fr>
2016-01-05 16:04:49 +0100

    Use watch on less to improve workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
 
-gulp.task('less', function() {
-  return gulp.src('./public/less/**/*.less')
-    .pipe(less({
-       paths: [ path.join(__dirname, 'dist', 'vendors') ]
-    }))
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('less:build', function() {
+  return gulp.src('./public/less/*.less')
+    .pipe(less({ paths: [ path.join(__dirname, 'dist', 'vendors') ]}))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(livereload());
+});
+
+gulp.task('less:watch', function() {
+    gulp.watch('./public/less/**/*.less', ['less:build']);  // Watch all the .less files, then run the less task
 });
