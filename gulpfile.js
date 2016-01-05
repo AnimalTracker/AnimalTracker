06b45950c4294d20ac0eafc9697df2d4cea7ee7c
Fanaen <contact@fanaen.fr>
2016-01-05 17:19:12 +0100

    Remove LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
-var livereload = require('gulp-livereload');
 var watch = require('gulp-watch');
+
+// Process style --
 
 gulp.task('less:build', function() {
   return gulp.src('./public/less/*.less')
     .pipe(less({ paths: [ path.join(__dirname, 'dist', 'vendors') ]}))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(livereload());
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('less:watch', function() {
     gulp.watch('./public/less/**/*.less', ['less:build']);  // Watch all the .less files, then run the less task
 });
+
+// Process source --
+
+gulp.task('serve', function () {
+    gulp.start(['less:watch']);
+});
+
+gulp.task('default', ['serve']);
