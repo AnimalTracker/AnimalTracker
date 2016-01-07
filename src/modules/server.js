8c17f833a655bda06d6969d3f64924647bd7e331
Fanaen <contact@fanaen.fr>
2016-01-07 09:29:31 +0100

    Refactor server module

--- src/modules/server.js
+++ src/modules/server.js
@@ -5,38 +5,13 @@ var config  = require('config');
 var express = require('express');
 var path = require('path');
 
+// -- Constants --
+
 var DEFAULT_PORT = 3000;
 
-// Optional parameters --
-exports.init = function(app) {
+// -- Methods --
 
-  if(config.has('server')) {
-    var server = config.get('server');
-
-    // Static file server --
-    if(server.has('static') && server.get('static')) {
-      app.use(express.static(path.join(__dirname, 'public')));
-    }
-
-    // Live reload --
-    if(server.has('liveReload') && server.get('liveReload')) {
-      var livereload = require('livereload');
-      var serverLr = livereload.createServer();
-      serverLr.watch(__dirname + '/public');
-    }
-  }
-
-  // Start server --
-  var server = app.listen(config.getPort(), function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('[app] GeneTracker listening at http://%s:%s', host, port);
-  });
-};
-
-// Safely get the port number --
-exports.getPort = function() {
+var getPort = function() {
   if(config.has('server')) {
     var server = config.get('server');
 
@@ -54,6 +29,45 @@ exports.getPort = function() {
   }
 
   // Fallback --
-  console.log('[config] no given port, fallback to default port');
+  console.log('[config] no server.port field, fallback to default port');
   return DEFAULT_PORT;
-}
+};
+
+var startStaticServer = function(app) {
+  app.use(express.static(path.join(__dirname, 'public')));
+};
+
+var startLiveReloadServer = function() {
+  var livereload = require('livereload');
+  var serverLr = livereload.createServer();
+  serverLr.watch(__dirname + '/public');
+};
+
+var startExpressServer = function(app) {
+  var server = app.listen(getPort(), function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('[app] GeneTracker listening at http://%s:%s', host, port);
+  });
+};
+
+// -- Module requirements --
+
+exports.init = function(app) {
+
+  if(config.has('server')) {
+    var server = config.get('server');
+
+    // Static file server --
+    if(server.has('static') && server.get('static'))
+      startStaticServer(app);
+
+    // Live reload --
+    if(server.has('liveReload') && server.get('liveReload'))
+      startLiveReloadServer();
+  }
+
+  // Express server --
+  startExpressServer(app);
+};
