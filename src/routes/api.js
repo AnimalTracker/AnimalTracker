e283df31acb6dbdeae30941a2d5644a0f23edc57
Matthieu Bronner <matthieu.bronner@viacesi.fr>
2016-01-13 13:49:00 +0100

    Fix get one user / add post user api

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,13 +6,13 @@ var express = require('express'),
     db = require('../modules/database');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json({
     message: 'Hello world!'
   });
 });
 
-router.get('/users', function(req, res, next) {
+router.get('/users', function(req, res) {
   db.select('first_name', 'last_name', 'username').from('User').where({active: true}).all()
     .then(function (users) {
       res.json({
@@ -22,12 +22,16 @@ router.get('/users', function(req, res, next) {
 });
 
 router.get('/users/:username', function (req, res) {
-    db.select().from('User').where({active: true, 'username': req.param('username')}).all()
-      .then(function (user) {
-        res.json({
-          user
-        });
+  db.select().from('User').where({active: true, 'username': req.param('username')}).one()
+    .then(function (user) {
+      res.json({
+        user
       });
+    });
+});
+
+router.post('/users', function(req, res) {
+
 });
 
 module.exports = router;
